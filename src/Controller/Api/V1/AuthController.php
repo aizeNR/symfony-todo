d4fb1c56929f5b5647870c02f38bc32c7c4ce26a
Kraev Tagir <kraev30@mail.ru>
2021-09-02 14:13:10 +0400

    refactoring

--- src/Controller/Api/V1/AuthController.php
+++ src/Controller/Api/V1/AuthController.php
@@ -3,14 +3,9 @@
 namespace App\Controller\Api\V1;
 
 use App\DTO\User\CreateUserDTO;
-use App\Entity\User;
-use App\Services\UserService;
 use App\UseCase\User\CreateUserAction;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends BaseController
